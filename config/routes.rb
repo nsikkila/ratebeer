159836459bb5d6b6584bdab5896e2d91d199b16b
Niko Sikkilä <nsikkila@localhost.Home>
2014-01-18 19:43:35 +0200

    breweries start page

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Ratebeer::Application.routes.draw do
 
   resources :breweries
 
+  root 'breweries#index'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
