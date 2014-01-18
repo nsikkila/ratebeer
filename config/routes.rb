9dca7804e30e0b36bf8977df9b07a390a55c06c0
Niko Sikkilä <niko.sikkila@gmail.com>
2014-01-18 20:00:30 +0200

    ratingscontroller and view

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,10 @@ Ratebeer::Application.routes.draw do
 
   root 'breweries#index'
 
+  get 'ratings', to: 'ratings#index'
+
+  # get 'kaikki_bisset', to: 'beers#index'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
