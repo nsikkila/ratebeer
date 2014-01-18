8484acf853acb8ce6316aa4a60ed1dcc11ccbf94
Niko Sikkilä <niko.sikkila@gmail.com>
2014-01-18 20:43:14 +0200

    ratings on beer pages

--- app/models/beer.rb
+++ app/models/beer.rb
@@ -1,4 +1,14 @@
 class Beer < ActiveRecord::Base
 	belongs_to :brewery
   has_many :ratings
+
+  def average_rating
+    sum = 0
+    count = 0
+    ratings.each do |rating|
+      sum += rating.score
+      count += 1
+    end
+    1.0*sum / count
+  end
 end
