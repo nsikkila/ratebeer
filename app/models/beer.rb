6566af61c18962d33e69c875e39c3350e6c53dfa
Niko Sikkilä <niko.sikkila@gmail.com>
2014-01-20 21:25:03 +0200

    beer to_s

--- app/models/beer.rb
+++ app/models/beer.rb
@@ -6,4 +6,8 @@ class Beer < ActiveRecord::Base
     ratings.inject(0) { | sum, rating | sum + rating.score } / ratings.count.to_f
     #ratings.average("score")
   end
+
+  def to_s
+    "#{brewery.name} - #{self.name}"
+  end
 end
