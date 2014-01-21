dddee8200b155c5f262d30f1da72029127250de4
Niko Sikkilä <niko.sikkila@gmail.com>
2014-01-21 19:21:37 +0200

    resources :rating

--- config/routes.rb
+++ config/routes.rb
@@ -5,11 +5,7 @@ Ratebeer::Application.routes.draw do
 
   root 'breweries#index'
 
-  get 'ratings', to:'ratings#index'
-
-  get 'ratings/new', to:'ratings#new'
-
-  post 'ratings', to:'ratings#create'
+  resources :ratings, :only => [:index, :new, :create]
 
   # get 'kaikki_bisset', to: 'beers#index'
 
