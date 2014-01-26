bd438dfd0ac42010e341c2aea0e03405d8964f22
Niko Sikkilä <niko.sikkila@gmail.com>
2014-01-26 18:44:26 +0200

    salasanan validointi

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,7 @@ class User < ActiveRecord::Base
   has_secure_password
 
   validates :username, uniqueness: true, length: { minimum: 3, maximum: 15}
+  validates :password, length: { minimum: 4 }, format: { with: /\A.*[A-Z].*\z/, with:  /\A.*[0-9].*\z/}
 
   has_many :ratings
   has_many :beers, through: :ratings
