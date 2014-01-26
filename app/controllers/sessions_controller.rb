8284d134b473329d6754bc1a9a72767379eba3b0
Niko Sikkilä <niko.sikkila@gmail.com>
2014-01-26 16:09:31 +0200

    kirjautumisen hienosäätöä

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,10 +6,15 @@ class SessionsController < ApplicationController
   def create
     # haetaan usernamea vastaava käyttäjä tietokannasta
     user = User.find_by username: params[:username]
-    #tallettaan sessioon kirjautuneen käyttäjän id
-    session[:user_id] = user.id if not user.nil?
-    # uudelleenohjataan käyttäjä omalle sivulleen
-    redirect_to user
+
+    if user.nil?
+      redirect_to :back, notice: "User #{params[:username]} does not exist!"
+    else
+      #tallettaan sessioon kirjautuneen käyttäjän id
+      session[:user_id] = user.id
+      # uudelleenohjataan käyttäjä omalle sivulleen
+      redirect_to user
+    end
   end
 
   def destroy
