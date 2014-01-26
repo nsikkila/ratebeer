0387aa92cc378bb2a48f500838e7c8b53eddf505
Niko Sikkilä <niko.sikkila@gmail.com>
2014-01-26 16:37:56 +0200

    panimon nimen validointi: ei tyhjä

--- app/models/brewery.rb
+++ app/models/brewery.rb
@@ -1,6 +1,8 @@
 class Brewery < ActiveRecord::Base
   include RatingAverage
 
+  validates :name, presence: true
+
 	has_many :beers, :dependent => :destroy
   has_many :ratings, :through => :beers
 
