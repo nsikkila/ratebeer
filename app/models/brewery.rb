37b615b655550f185be00720f30540917d1818d4
Niko Sikkilä <niko.sikkila@gmail.com>
2014-01-26 16:39:20 +0200

    panimon vuoden validointi: 1042-2014

--- app/models/brewery.rb
+++ app/models/brewery.rb
@@ -2,6 +2,7 @@ class Brewery < ActiveRecord::Base
   include RatingAverage
 
   validates :name, presence: true
+  validates :year, numericality: { less_than_or_equal_to: 2014, greater_than_or_equal_to: 1042}
 
 	has_many :beers, :dependent => :destroy
   has_many :ratings, :through => :beers
