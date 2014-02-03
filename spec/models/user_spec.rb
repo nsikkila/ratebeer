84940b034c9e9300680e4c793fb2a7c5af99be48
Niko Sikkilä <niko.sikkila@gmail.com>
2014-02-03 11:54:27 +0200

    uus testi

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -20,4 +20,17 @@ describe User do
   	expect(user.valid?).to be(true)
   	expect(User.count).to eq(1)
   end
+
+  it "with a proper password and two ratings, has the correct average rating" do
+  	user = User.create username:"Pekka", password:"Secret1", password_confirmation:"Secret1"
+  	rating = Rating.new score:10
+  	rating2 = Rating.new score:20
+
+  	user.ratings << rating
+  	user.ratings << rating2
+
+  	expect(user.ratings.count).to eq(2)
+  	expect(user.average_rating).to eq(15)
+  end
+
 end
