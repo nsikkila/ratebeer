7b78a792ee8e59365c32a494ff77403769af7743
Niko Sikkilä <niko.sikkila@gmail.com>
2014-02-03 17:00:20 +0200

    before :each

--- spec/features/brewery_page_spec.rb
+++ spec/features/brewery_page_spec.rb
@@ -8,35 +8,33 @@ describe "Breweries page" do
 		expect(page).to have_content 'Number of breweries: 0'
 	end
 
-	it "lists the existing breweries and their total number" do
-		breweries = ["Koff", "Karjala", "Schlenkerla"]
-		breweries.each do | brewery_name |
-			FactoryGirl.create(:brewery, name:brewery_name)
+	describe "when breweries exist" do
+		before :each do
+			@breweries = ["Koff", "Karjala", "Schlenkerla"]
+ 			year = 1896
+			breweries.each do | brewery_name |
+				FactoryGirl.create(:brewery, name:brewery_name, year: year += 1)
+			end
+
+			visit breweries_path
 		end
 
-		visit breweries_path
+		it "lists the breweries and their total number" do
 
-		expect(page).to have_content "Number of breweries: #{breweries.count}"
-	
-		breweries.each do | brewery_name |
-			expect(page).to have_content brewery_name
+			expect(page).to have_content "Number of breweries: #{breweries.count}"
+		
+			breweries.each do | brewery_name |
+				expect(page).to have_content brewery_name
+			end
+
 		end
 
+		it "allows user to navigate to page of a brewery" do
+
+			click_link "Koff"
+
+			expect(page).to have_content "Koff"
+			expect(page).to have_content "Established year: 1897"
+		end
 	end
-
-	it "allows user to navigate to page of a brewery" do
-		breweries = ["Koff", "Karjala", "Schlenkerla"]
-		year = 1896
-		breweries.each do | brewery_name |
-			FactoryGirl.create(:brewery, name:brewery_name, year: year += 1)
-		end
-
-		visit breweries_path
-
-		click_link "Koff"
-
-		expect(page).to have_content "Koff"
-		expect(page).to have_content "Established year: 1897"
-	end
-
 end
