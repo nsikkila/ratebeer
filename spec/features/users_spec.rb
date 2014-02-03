b8dba061081d69b9cbf0f0a03873eacae2ff12f2
Niko Sikkilä <niko.sikkila@gmail.com>
2014-02-03 17:23:43 +0200

    testi: rekisteröityminen lisää käyttäjien määrää yhdellä

--- spec/features/users_spec.rb
+++ spec/features/users_spec.rb
@@ -1,11 +1,23 @@
 require 'spec_helper'
 
 describe "User" do
-	before :each do
-		FactoryGirl.create :user
+
+
+	it "when signed up with good credentials, is added to the system" do
+		visit signup_path
+		fill_in('user_username', with:'Brian')
+		fill_in('user_password', with:'Secret55')
+		fill_in('user_password_confirmation', with:'Secret55')
+
+		expect{
+			click_button('Create User')
+		}.to change{User.count}.by(1)
 	end
 
 	describe "who has signed up" do
+		before :each do
+			FactoryGirl.create :user
+		end
 		
 		it "can sign in with right credentials" do
 			visit signin_path
