c80cf967e8dae405bc0ccbcf7691f1a654f4b7d8
Niko Sikkilä <niko.sikkila@gmail.com>
2014-02-03 11:59:19 +0200

    let

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,36 +1,38 @@
 require 'spec_helper'
 
 describe User do
-  it "has the username set correctly" do
-  	user = User.new username:"Pekka"
+	it "has the username set correctly" do
+		user = User.new username:"Pekka"
 
-  	user.username.should == "Pekka"
-  end
+		user.username.should == "Pekka"
+	end
 
-  it "is not saved without a password" do
-  	user = User.create username:"Pekka"
+	it "is not saved without a password" do
+		user = User.create username:"Pekka"
 
-  	expect(user.valid?).to be(false)
-  	expect(User.count).to eq(0)
-  end
+		expect(user.valid?).to be(false)
+		expect(User.count).to eq(0)
+	end
 
-  it "is saved with a proper password" do
-  	user = User.create username:"Pekka", password:"Secret1", password_confirmation:"Secret1"
-  
-  	expect(user.valid?).to be(true)
-  	expect(User.count).to eq(1)
-  end
+	describe "with a proper password" do
+		let(:user){ User.create username:"Pekka", password:"Secret1", password_confirmation:"Secret1" }
 
-  it "with a proper password and two ratings, has the correct average rating" do
-  	user = User.create username:"Pekka", password:"Secret1", password_confirmation:"Secret1"
-  	rating = Rating.new score:10
-  	rating2 = Rating.new score:20
+		it "is saved" do
 
-  	user.ratings << rating
-  	user.ratings << rating2
+			expect(user.valid?).to be(true)
+			expect(User.count).to eq(1)
+		end
 
-  	expect(user.ratings.count).to eq(2)
-  	expect(user.average_rating).to eq(15)
-  end
+		it "and two ratings, has the correct average rating" do
+			rating = Rating.new score:10
+			rating2 = Rating.new score:20
+
+			user.ratings << rating
+			user.ratings << rating2
+
+			expect(user.ratings.count).to eq(2)
+			expect(user.average_rating).to eq(15)
+		end
+	end
 
 end
