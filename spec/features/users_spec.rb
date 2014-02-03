b3366a87092a79a8b48699056325862c10261950
Niko Sikkilä <niko.sikkila@gmail.com>
2014-02-03 17:16:37 +0200

    testi: käyttäjä ohjataan takaisin kirjautumiseen, jos antaa väärät credentialit

--- spec/features/users_spec.rb
+++ spec/features/users_spec.rb
@@ -5,7 +5,7 @@ describe "User" do
 		FactoryGirl.create :user
 	end
 
-	describe "who has signed in" do
+	describe "who has signed up" do
 		
 		it "can sign in with right credentials" do
 			visit signin_path
@@ -15,6 +15,16 @@ describe "User" do
 
 			expect(page).to have_content 'Welcome back!'
 			expect(page).to have_content 'Pekka'
+		end
+
+		it "is redirected back to signin form if wrong credentials given" do
+			visit signin_path
+			fill_in('username', with:'Pekka')
+			fill_in('password', with:'wrong')
+			click_button('Log in')
+
+			expect(current_path).to eq(signin_path)
+			expect(page).to have_content 'Incorrect username or password'
 		end
 
 	end
