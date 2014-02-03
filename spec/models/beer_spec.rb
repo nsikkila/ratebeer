5a7d582fcea30013ca370102b5365bd92b23045c
Niko Sikkilä <niko.sikkila@gmail.com>
2014-02-03 12:16:18 +0200

    olut ei tallennu ilman nimeä

--- spec/models/beer_spec.rb
+++ spec/models/beer_spec.rb
@@ -1,12 +1,20 @@
 require 'spec_helper'
 
 describe Beer do
-  describe "with proper name and style" do
-  	let(:beer){ Beer.create name:"Testiolut", style:"Lager" }
 
-  	it "is saved" do
-  		expect(beer.valid?).to be(true)
-  		expect(Beer.count).to eq(1)
-  	end
-  end
+	it "is not saved without a name" do
+		beer = Beer.create style:"Lager"
+
+		expect(beer.valid?).to be(false)
+		expect(Beer.count).to eq(0)
+	end
+
+  	describe "with proper name and style" do
+  		let(:beer){ Beer.create name:"Testiolut", style:"Lager" }
+
+	  	it "is saved" do
+	  		expect(beer.valid?).to be(true)
+	  		expect(Beer.count).to eq(1)
+	  	end
+	end
 end
