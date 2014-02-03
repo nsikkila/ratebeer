4af8827c85d34c90389cfd339df1d68df668d32e
Niko Sikkilä <niko.sikkila@gmail.com>
2014-02-03 16:42:27 +0200

    ensimmäinen selaintason testi

--- Gemfile
+++ Gemfile
@@ -13,6 +13,8 @@ end
 
 group :test do
 	gem 'factory_girl_rails'
+	gem 'capybara'
+	gem 'launchy'
 end
 
 
---
+++ spec/features/brewery_page_spec.rb
@@ -0,0 +1,10 @@
+require 'spec_helper.rb'
+
+describe "Breweries page" do
+
+	it "should not have any before been created" do
+		visit breweries_path
+		expect(page).to have_content 'Listing breweries'
+		expect(page).to have_content 'Number of breweries: 0'
+	end
+end
