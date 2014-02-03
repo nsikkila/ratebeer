106ce66f0e15523818abf08df2e050375b2f0ba5
Niko Sikkilä <niko.sikkila@gmail.com>
2014-02-03 12:18:31 +0200

    testi: olut ei tallennu, jos tyyliä ei ole asetettu

--- spec/models/beer_spec.rb
+++ spec/models/beer_spec.rb
@@ -9,8 +9,15 @@ describe Beer do
 		expect(Beer.count).to eq(0)
 	end
 
+	it "is not saved without a style" do
+		beer = Beer.create name:"Testbeer"
+
+		expect(beer.valid?).to be(false)
+		expect(Beer.count).to eq(0)
+	end
+
   	describe "with proper name and style" do
-  		let(:beer){ Beer.create name:"Testiolut", style:"Lager" }
+  		let(:beer){ Beer.create name:"Testbeer", style:"Lager" }
 
 	  	it "is saved" do
 	  		expect(beer.valid?).to be(true)
