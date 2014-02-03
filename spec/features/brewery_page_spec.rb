2178ace9c9af1b0ee404b85ded1099972bc2dbe8
Niko Sikkilä <niko.sikkila@gmail.com>
2014-02-03 16:50:49 +0200

    testi: panimosivu listaa panimot ja niiden lukumäärän

--- spec/features/brewery_page_spec.rb
+++ spec/features/brewery_page_spec.rb
@@ -7,4 +7,21 @@ describe "Breweries page" do
 		expect(page).to have_content 'Listing breweries'
 		expect(page).to have_content 'Number of breweries: 0'
 	end
+
+	it "lists the existing breweries and their total number" do
+		breweries = ["Koff", "Karjala", "Schlenkerla"]
+		breweries.each do | brewery_name |
+			FactoryGirl.create(:brewery, name:brewery_name)
+		end
+
+		visit breweries_path
+
+		expect(page).to have_content "Number of breweries: #{breweries.count}"
+	
+		breweries.each do | brewery_name |
+			expect(page).to have_content brewery_name
+		end
+
+	end
+
 end
