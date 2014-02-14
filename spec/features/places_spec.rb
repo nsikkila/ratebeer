fd848b2348e8a67b11316695a1437e28a0c9dd81
Niko Sikkilä <niko.sikkila@gmail.com>
2014-02-14 19:23:46 +0200

    places-sivun testit

--- spec/features/places_spec.rb
+++ spec/features/places_spec.rb
@@ -13,4 +13,31 @@ describe "Places" do
 
     expect(page).to have_content "Oljenkorsi"
   end
+
+  it "if multiple are returned by the API, all are shown on the page" do
+
+    BeermappingApi.stub(:places_in).with("kumpula").and_return(
+        [ Place.new(:name => "Oljenkorsi"), Place.new(:name => "Koljenorsi"), Place.new(:name => "Korsinolji")]
+    )
+
+    visit places_path
+    fill_in('city', with: 'kumpula')
+    click_button 'Search'
+
+    expect(page).to have_content 'Oljenkorsi'
+    expect(page).to have_content 'Koljenorsi'
+    expect(page).to have_content 'Korsinolji'
+  end
+
+  it "if none are returned by the API, notice is shown" do
+
+    BeermappingApi.stub(:places_in).with('kumpula').and_return([])
+
+    visit places_path
+    fill_in('city', with: 'kumpula')
+    click_button 'Search'
+
+    expect(page).to have_content 'No places in kumpula'
+
+  end
 end
