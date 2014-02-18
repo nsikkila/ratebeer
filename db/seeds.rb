01d5ee345824e384d420b1f427868adecf2de689
Niko Sikkilä <niko.sikkila@gmail.com>
2014-02-18 11:00:38 +0200

    pikkukorjauksia

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,13 +12,19 @@ br1 = Brewery.create name:"Koff", year:1897
 br2 = Brewery.create name:"Malmgard", year:2001
 br3 = Brewery.create name:"Weihenstephaner", year:1042
 
-b1 = Beer.create name:"Iso 3", style:"Lager", brewery_id:br1.id
-b2 = Beer.create name:"Karhu", style:"Lager", brewery_id:br1.id
-b3 = Beer.create name:"Tuplahumala", style:"Lager", brewery_id:br1.id
-b4 = Beer.create name:"Huvila Pale Ale", style:"Pale Ale", brewery_id:br2.id
-b5 = Beer.create name:"X Porter", style:"Porter", brewery_id:br2.id
-b6 = Beer.create name:"Hefezeizen", style:"Weizen", brewery_id:br3.id
-b7 = Beer.create name:"Helles", style:"Lager", brewery_id:br3.id
+s1 = Style.create name:"Lager", description:"Lageria"
+s2 = Style.create name:"IPA", description:"IPAa"
+s3 = Style.create name:"Porter", description:"Porteria"
+s4 = Style.create name:"Pale Ale", description:"Palea alea"
+s5 = Style.create name:"Weizen", description:"Weizenia"
+
+b1 = Beer.create name:"Iso 3", style_id:s1.id, brewery_id:br1.id
+b2 = Beer.create name:"Karhu", style_id:s1.id, brewery_id:br1.id
+b3 = Beer.create name:"Tuplahumala", style_id:s1.id, brewery_id:br1.id
+b4 = Beer.create name:"Huvila Pale Ale", style_id:s4.id, brewery_id:br2.id
+b5 = Beer.create name:"X Porter", style_id:s3.id, brewery_id:br2.id
+b6 = Beer.create name:"Hefezeizen", style_id:s5.id, brewery_id:br3.id
+b7 = Beer.create name:"Helles", style_id:s1.id, brewery_id:br3.id
 
 u1 = User.create username:"user1", password:"User1", password_confirmation:"User1"
 u2 = User.create username:"user2", password:"User2", password_confirmation:"User2"
